70a3e0a8d66cabd5ed650a65e17096ce7c403fcb
Dustin Clifford <dclifford@sitesoftllc.net>
2019-12-01 12:28:11 -0800

    trying to fix body parsing in function

--- helloWorldHttpTrigger/index.js
+++ helloWorldHttpTrigger/index.js
@@ -13,7 +13,7 @@ module.exports = async function (context, req) {
     else {
         context.res = {
             status: 400,
-            body: "Please pass a name on the query string or in the request body"
+            body: "Please pass a name on the query string or in the request body " + req.body
         };
     }
 };
